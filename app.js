fa504378d2ad7cf1f0dcc05ab26fa44676eabd4f
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-21 15:14:44 -0300

    router config

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 // Middlewares
 const notFoundMiddleware = require("./middleware/not-found");
@@ -18,6 +19,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
