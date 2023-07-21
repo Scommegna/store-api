035e16e82da1d6c8fa37a1d4219c0afa266141a8
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-21 16:54:26 -0300

    added queryObject logic

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,19 @@
+const Products = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products testing route" });
+  const products = await Products.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products route" });
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  const products = await Products.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
