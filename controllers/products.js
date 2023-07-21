2fee57088d9dbf100cb3459f7487c99fadf253af
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-21 17:13:21 -0300

    added query search by name with regex logic

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,27 @@
 const Products = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Products.find({ featured: true });
+  const search = "a";
+  const products = await Products.find({
+    name: { $regex: search, $options: "i" },
+  });
   res.status(200).json({ products, nbHits: products.length });
 };
+
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
 
   const products = await Products.find(queryObject);
