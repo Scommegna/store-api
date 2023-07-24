97b11f2c6d0bdaa002729eee98e002ed1c74636b
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-24 14:03:20 -0300

    sorting, select by field and pagination logic

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,13 @@
 const Products = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "a";
-  const products = await Products.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Products.find({}).sort("name").select("name price");
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -24,7 +22,28 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  const products = await Products.find(queryObject);
+  let result = Products.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
